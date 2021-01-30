7f9fa8b2aee5dbc25fe87c84dbeebec723842cac
busraakbulut <busraakbulut@github.com>
2021-01-30 17:34:50 +0300

    Deneme

---
+++ OOP2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
