9886cd18cb625098da111137742035715f78ccbb
busraakbulut <busraakbulut@github.com>
2021-02-04 23:36:41 +0300

    Yeni dosyalar

---
+++ Constructor/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer {Id= 1, FirstName= "Büşra", LastName = "AKBULUT", City= "ANTALYA" };
+
+            Customer customer2 = new Customer(2,"Engin","Demiroğ","Diyarbakır");
+        }
+    }
+    class Customer
+    {
+        // kendi oluşturduğumuz constru
+        public Customer(int id, string firstName, string lastName, string city)//
+        {
+
+        }
+        // default constructordır yazmamıza gerek yoktur 
+        public Customer() // bu bir constructor (ctor tab*2)
+        {
+
+        }
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+
+    }
+}
